aea5b711948687dcaf67913dad3291d9a2ba9fb2
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2021-11-30 19:02:52 +0100

    Fix login

--- login.py
+++ login.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-		return "Hallo "+request.form['username']+"!"
+        return "Hallo "+request.form['username']+"!"
     else:
-		return '''
-		<h2>Come ti chiami?</h2>
-			<form action="" method="post">
-				<input type="text" name="username">
-				<input type="submit" value="Invia">
-			</form>
-			'''
+        return '''
+            <h2>Come ti chiami?</h2>
+		<form action="" method="post">
+			<input type="text" name="username">
+			<input type="submit" value="Invia">
+		</form>
+		'''
